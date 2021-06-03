6ca964d77d9e63ba6e78634a3753cd72cbd6e11f
a8ifx2w2q <85213210+a8ifx2w2q@users.noreply.github.com>
2021-06-03 11:11:17 +0530

    Update start.py

--- start.py
+++ start.py
@@ -7,15 +7,16 @@ from webdriver_manager.chrome import ChromeDriverManager
 weburl = os.getenv('WEB_URL')
 
 gChromeOptions = webdriver.ChromeOptions()
+gChromeOptions.add_argument("--no-sandbox")
 gChromeOptions.add_argument("--window-size=1920x1480")
 gChromeOptions.add_argument("--disable-dev-shm-usage")
 gChromeOptions.add_argument("--incognito")
 gChromeOptions.add_argument("--headless")
-gChromeOptions.add_argument("--no-sandbox")
 gDriver1 = webdriver.Chrome(options=gChromeOptions, executable_path=ChromeDriverManager().install())
 gDriver2 = webdriver.Chrome(options=gChromeOptions, executable_path=ChromeDriverManager().install())
 gDriver3 = webdriver.Chrome(options=gChromeOptions, executable_path=ChromeDriverManager().install())
 gDriver4 = webdriver.Chrome(options=gChromeOptions, executable_path=ChromeDriverManager().install())
+gDriver5 = webdriver.Chrome(options=gChromeOptions, executable_path=ChromeDriverManager().install())
 check=1
 while(check>0):
     try:
@@ -23,11 +24,13 @@ while(check>0):
         gDriver2.get(weburl)
         gDriver3.get(weburl)
         gDriver4.get(weburl)
+        gDriver5.get(weburl)
         time.sleep(30)
         gDriver1.close()
         gDriver2.close()
         gDriver3.close()
         gDriver4.close()
+        gDriver5.close()
         print("Times Run = ", check)
         check=check+1
     except:
